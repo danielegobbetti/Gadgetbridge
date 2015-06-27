417ea023475aacde3af305882e8fc687f117a7e9
Daniele Gobbetti <daniele@gobbetti.name>
2015-06-27 18:50:21 +0200

    Test to see if incrementing the day will make the alarm fire. It's yet to check if the Calendar class will deal with months/years change all by itself.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/GBAlarm.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/GBAlarm.java
@@ -120,9 +120,15 @@ public class GBAlarm implements Parcelable, Comparable {
         return this.minute;
     }
     public Calendar getAlarmCal() {
+
         Calendar alarm = Calendar.getInstance();
+        Calendar now = Calendar.getInstance();
         alarm.set(Calendar.HOUR_OF_DAY, this.hour);
         alarm.set(Calendar.MINUTE, this.minute);
+        if (now.after(alarm) && repetition == ALARM_ONCE) {
+            //if the alarm is in the past set it to tomorrow
+            alarm.add(Calendar.DATE, 1);
+        }
         return alarm;
     }
 
